fa3d93c019dfcb8cc15f68d078324933c04e37c8
James Grieve <pocojames@hotmail.com>
2020-05-24 01:38:09 -0700

    create add_score method and lose? method for player class, create take_turn method and refactor valid? method for game class

--- game.rb
+++ game.rb
@@ -1,26 +1,40 @@
 require_relative "player.rb"
+require "set"
 class Game
     attr_reader :dictionary
     attr_accessor :fragment, :players
     def initialize(*players)
         @players = players.map {|p| Player.new(p)}
-        @dictionary = ["yolk","potato","apple"] #using these words for testing purposes
-        # @dictionary = File.readlines("dictionary.txt").map(&:chomp)
+        @dictionary = Set["yolk","potato","apple"] #using these words for testing purposes
+        # @dictionary = Set[File.readlines("dictionary.txt").map(&:chomp)]
         @fragment = "yol"
+    end
+
+    #now need methods for gameplay/guessing
+    def take_turn
+        puts "Enter a letter"
+        letter = gets.chomp
+        if valid?(letter)
+            puts "valid, goes to next player"
+        else
+            #lose condition
+            #current_player loses
+            puts "you lose!"
+        end
     end
 
     def valid?(letter)
         frag = fragment
         frag += letter
-        if dictionary.include?(frag)
-            dictionary += letter
-            true
-        else
+        if dictionary.include?(frag) && !lose?(frag)
             false
+        else
+            @fragment += letter
+            true
         end
     end
 
-    def lose?
+    def lose?(word)
         dictionary.each do |word|
             if word == fragment
                 return true
@@ -37,7 +51,5 @@ class Game
     def current_player
         players.first
     end 
-
-
 end
 
--- player.rb
+++ player.rb
@@ -1,6 +1,37 @@
 class Player
-    attr_accessor :name
+    attr_accessor :name, :score
     def initialize(name)
         @name = name
+        @score = []
+    end
+
+    def add_score
+        length = @score.length 
+
+        case length
+        when 0
+            @score << "G"
+            true
+        when 1
+            @score << "H"
+            true
+        when 2
+            @score << "O"
+            true
+        when 3
+            @score << "S"
+            true
+        when 4
+            @score << "T"
+            false
+        else
+            false
+        end
+    end
+
+    def lose?
+       if !add_score
+        puts "lose!"
+       end
     end
 end
